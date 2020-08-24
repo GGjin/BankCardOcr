d61bb3250303b890eaf312ebf4704fa43107a7ac
GGjin <gg.jin.yu@gmail.com>
2020-08-24 17:39:55 +0800

    Initial commit

---
+++ bankcardocrtest/src/main/java/com/gg/bankcardocr/test/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.gg.bankcardocr.test
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ *  Create by GG on 2020/8/24
+ *  mail is gg.jin.yu@gmail.com
+ */
+class MainActivity :AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+}
